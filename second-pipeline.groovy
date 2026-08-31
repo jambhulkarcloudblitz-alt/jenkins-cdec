ed61cf53c2b66be42950220015f0a3586afab0a3
shubhamj <shubham.cloudblitz@gmail.com>
2026-08-31 10:10:21 +0530

    s3-upload

--- second-pipeline.groovy
+++ second-pipeline.groovy
@@ -13,22 +13,27 @@ pipeline {
                         '''
             }
         }
-        stage ('TEST') {
-            steps {
-                sh '''cd backend
-                        mvn org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
-                        -Dsonar.projectKey=studentapp \
-                        -Dsonar.projectName=studentapp \
-                        -Dsonar.host.url=http://54.88.140.52:9000 \
-                        -Dsonar.token='YOUR_TOKEN'
+        // stage ('TEST') {
+        //     steps {
+        //         sh '''cd backend
+        //                 mvn org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
+        //                 -Dsonar.projectKey=studentapp \
+        //                 -Dsonar.projectName=studentapp \
+        //                 -Dsonar.host.url=http://54.88.140.52:9000 \
+        //                 -Dsonar.token='YOUR_TOKEN'
 
-                        '''
-            }
-        }
-        stage ('DEPLOY') {
+        //                 '''
+        //     }
+        // }
+        stage ('TEST'){
             steps {
-                sh 'echo "DEPLOY SUCCESS"'
+                sh 'echo "TEST SUCCESS"'
             }
         }
+        // stage ('S3-Upload') {
+        //     steps {
+        //         sh 'aws s3 cp backed/target/ s3://s3-upload-6741/student.jar'
+        //     }
+        // }
     }
 }
