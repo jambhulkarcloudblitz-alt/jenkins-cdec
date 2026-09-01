428d3535f6e626f3d80f2dda97e62323d2ad2150
shubhamj <shubham.cloudblitz@gmail.com>
2026-09-01 09:59:19 +0530

    fix

--- second-pipeline.groovy
+++ second-pipeline.groovy
@@ -13,26 +13,30 @@ pipeline {
                         '''
             }
         }
-        // stage ('TEST') {
-        //     steps {
-        //         sh '''cd backend
-        //                 mvn org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
-        //                 -Dsonar.projectKey=studentapp \
-        //                 -Dsonar.projectName=studentapp \
-        //                 -Dsonar.host.url=http://54.88.140.52:9000 \
-        //                 -Dsonar.token='YOUR_TOKEN'
-
-        //                 '''
-        //     }
-        // }
-        stage ('TEST'){
+        stage ('TEST') {
             steps {
-                sh 'echo "TEST SUCCESS"'
+                sh '''cd backend
+                 mvn org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \\
+                -Dsonar.projectKey=studentapp \\
+                -Dsonar.projectName=studentapp \\
+                -Dsonar.host.url=http://3.92.63.71:9000  \\
+                -Dsonar.token=sqp_2eb5833ca50b9121d0fd14a09f160197873e495e'''
+
             }
         }
-        stage ('S3-Upload') {
+        // stage ('TEST'){
+        //     steps {
+        //         sh 'echo "TEST SUCCESS"'
+        //     }
+        // }
+        // stage ('S3-Upload') {
+        //     steps {
+        //         sh 'aws s3 cp backend/target/student-registration-backend-0.0.1-SNAPSHOT.jar s3://s3-upload-6741/student.jar'
+        //     }
+        // }
+        stage ('Deploy') {
             steps {
-                sh 'aws s3 cp backend/target/student-registration-backend-0.0.1-SNAPSHOT.jar s3://s3-upload-6741/student.jar'
+                sh 'echo "Deploy Succsess"'
             }
         }
     }
